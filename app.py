1546b0053ca83e131da235298277de5390dfeb0c
Carlos Enrique Jiménez Avendaño <caliche18a@gmail.com>
2026-05-15 22:46:45 -0500

    Se corrige host de ejecución

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ server = app.server
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8050"))
     debug = os.environ.get("DASH_DEBUG", "true").lower() == "true"
-    app.run(debug=debug, host="172.0.0.1", port=port)
+    app.run(debug=debug, host="0.0.0.0", port=port)
